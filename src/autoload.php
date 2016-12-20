6f303d11b1a42531f145dda9e7eabe23386cd1db
Sheldon Juncker <sheldonjuncker@gmail.com>
2016-12-20 10:32:46 -0500

    Changed the autoloader from a class to a closure.

--- src/autoload.php
+++ src/autoload.php
@@ -1,65 +1,47 @@
 <?php
 
 /**
-* A simple PHP PSR-4 compatible autoloader.
+* Loads a simple PSR-4 autoloader for the Validator.
 */
-class ValidatorAutoloader
-{
-	/**
-	* @var array $prefixes A mapping of namespace prefixes to base directories.
-	*/
-	protected $prefixes = [
+spl_autoload_register(function(string $class){
+	//A mapping of namespace prefixes to base directories.
+	$prefixes = [
 		'Validator' => 'src\\Validator',
 	];
 
-	/**
-	* Registers the autoloader with SPL.
-	*/
-	public function register()
-	{
-		spl_autoload_register([$this, 'loadClass']);
-	}
+	//Get list of namespaces, and unqualified class name
+	$namespaces = explode('\\', $className);
+	$class = array_pop($namespaces);
 
-	/**
-	* Loads a class.
-	* @param string $className
-	*/
-	public function loadClass(string $className)
+	//Find the longest prefix to use as the base path
+	$basePath = "";
+	$pathParts = [];
+	while(count($namespaces))
 	{
-		//Get list of namespaces, and unqualified class name
-		$namespaces = explode('\\', $className);
-		$class = array_pop($namespaces);
-
-		//Find the longest prefix to use as the base path
-		$basePath = "";
-		$pathParts = [];
-		while(count($namespaces))
+		$prefixStr = implode('\\', $namespaces);
+		if(isset($prefixes[$prefixStr]))
 		{
-			$prefixStr = implode('\\', $namespaces);
-			if(isset($this->prefixes[$prefixStr]))
-			{
-				$basePath = $this->prefixes[$prefixStr];
-				break;
-			}
-
-			else
-			{
-				array_unshift($pathParts, array_pop($namespaces));
-			}
+			$basePath = $prefixes[$prefixStr];
+			break;
 		}
 
-		//Found
-		if($basePath != "")
+		else
 		{
-			array_unshift($pathParts, $basePath);
-			array_push($pathParts, $class . '.php');
-			$file = implode(DIRECTORY_SEPARATOR, $pathParts);
-			if(file_exists($file))
-			{
-				require $file;
-			}
+			array_unshift($pathParts, array_pop($namespaces));
 		}
 	}
-}
+
+	//Found
+	if($basePath != "")
+	{
+		array_unshift($pathParts, $basePath);
+		array_push($pathParts, $class . '.php');
+		$file = implode(DIRECTORY_SEPARATOR, $pathParts);
+		if(file_exists($file))
+		{
+			require $file;
+		}
+	}
+});
 
 ?>
