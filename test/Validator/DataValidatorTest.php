3b7ccbc7b20288da1ab1acc29640a6bf5173c8a4
Sheldon Juncker <sheldonjuncker@gmail.com>
2016-12-21 17:55:16 -0500

    Adding tests for the DataValidator.

--- test/Validator/DataValidatorTest.php
+++ test/Validator/DataValidatorTest.php
@@ -1,8 +1,32 @@
 <?php
 
+use Validator\DataValidator;
+
 class DataValidatorTest extends \PHPUnit_Framework_TestCase
 {
-	
+	//Validation only fails if a rule fails, if there are no rules, none can fail
+	public function testEmptyRules()
+	{
+		$rules = [];
+		$validator = new DataValidator('data', $rules);
+		$this->assertTrue($validator->validate());
+	}
+
+	public function testValidation()
+	{
+		$validator = new DataValidator('data', [
+			'min-length' => 4
+		]);
+		$this->assertTrue($validator->validate());
+	}
+
+	public function testInvalidation()
+	{
+		$validator = new DataValidator('data', [
+			'min-length' => 5
+		]);
+		$this->assertFalse($validator->validate());
+	}
 }
 
 ?>
